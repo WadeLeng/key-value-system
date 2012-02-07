d43afafcc19c17ead85e2ffb0653ae33add47215
冷得维 <wade.hit@gmail.com>
2012-02-07 21:21:14 +0800

    更新Makefile,调整文件目录，源文件放到了/src里

---
+++ src/layout.h
@@ -0,0 +1,40 @@
+/*============================================================================
+# Author: Wade Leng
+# E-mail: wade.hit@gmail.com
+# Last modified:	2012-02-04 16:59
+# Filename:		layout.h
+# Description: 
+============================================================================*/
+#ifndef	KVS_LAYOUT
+#define	KVS_LAYOUT
+
+/* 1.SuperImage Layout:
+ * total 1 KB
+ * FIXME: Something may be here in the near future.
+ * may be used in function: kv_init() @ interface.c
+ * Magic Number? KVS instance version?
+ */
+
+/*2.IndexImage Layout:
+ * total 320002228 KB = 305.2MB
+ */
+#define	IMAGE_HT_TABLE			1024		//300 * 4	
+#define	IMAGE_IDX_NODES			2224		//1000w * 28
+#define	IMAGE_FREE_IDX_NODES		280002224	//1000w * 4
+#define	IMAGE_FREE_IDX_NODES_HORIZON	320002224	//4
+
+#define	IMAGE_SIZE			320002228
+
+#define	IDX_NODES_MAX			10000000
+#define	IDX_HT_TABLE_SIZE		300
+
+#define	DISK_IMAGE_OFFSET		0
+#define	DISK_VALUE_OFFSET		0			//320002228
+
+#define	VALUE_MAX_SIZE			(5*1024*1024)	// (5MB)
+
+#define	BUF_PTR_NULL			NULL
+#define	DISK_OFFSET_NULL		-1
+#define	IDX_NODE_NULL			-1
+
+#endif
---
+++ src/sync.c
@@ -0,0 +1,152 @@
+/*============================================================================
+# Author: Wade Leng
+# E-mail: wade.hit@gmail.com
+# Last modified:	2012-02-04 16:58
+# Filename:		sync.c
+# Description: 
+============================================================================*/
+#define	_FILE_OFFSET_BITS	64
+#define _LARGEFILE_SOURCE
+#define _LARGEFILE64_SOURCE
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <errno.h>
+#include <string.h>
+
+#include "type.h"
+#include "layout.h"
+#include "sync.h"
+
+static int fd = 0;
+extern FILE* log_file;
+
+int sync_init(char* pathname, int file_len)
+{
+	fd = open(pathname, O_RDWR | O_LARGEFILE | O_CREAT , S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP); 
+	if (fd <= 0) 
+		return -1;
+
+	fprintf(log_file, "SYNC INIT SUCCESS.\n");
+	return 0;
+}
+
+int sync_read(char* mem, int size, OFFSET_T disk_offset)
+{
+	off64_t ret;
+	ssize_t n = 0, toread;
+	char* ptr;
+
+	if (fd <= 0)
+		return -1;
+
+	ret = lseek64(fd, (off64_t)disk_offset, SEEK_SET);
+	if (ret < 0)
+		return -1;
+
+	toread = size;
+	ptr = mem;
+	while (toread > 0)
+	{
+		n = read(fd, ptr, size);
+		toread -= n;
+		ptr += n;
+	}
+
+	return 0;
+}
+
+int sync_write(const char* mem, int size, OFFSET_T disk_offset)
+{
+	off64_t ret;
+	ssize_t n, towrite;
+	const char* ptr;
+
+	if (fd <= 0) 
+		return -1;
+
+	ret = lseek64(fd, (off64_t)disk_offset, SEEK_SET);
+	if (ret < 0) 
+		return -1;
+
+	towrite = size;
+	ptr = mem;
+	while (towrite > 0)
+	{
+		n = write(fd, ptr, size); 
+		towrite -= n;
+		ptr += n;
+	}
+
+	return 0;
+}
+
+int sync_image_load(const char* file_name, char* g_image, int size)
+{
+	off_t ret;
+	ssize_t n, toread;
+	char* ptr;
+	int image_fd;
+
+	image_fd = open(file_name, O_RDWR , S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP); 
+	if (image_fd <= 0) 
+		return -1;
+
+	ret = lseek(image_fd, 0, SEEK_SET);
+	if (ret < 0)
+		return -1;
+
+	toread = size;
+	ptr = g_image;
+	while (toread > 0)
+	{
+		n = read(image_fd, ptr, size);
+		toread -= n;
+		ptr += n;
+	}
+
+	return 0;
+}
+
+int sync_image_save(const char* file_name, char* g_image, int size)
+{
+	off_t ret;
+	ssize_t n, towrite;
+	char* ptr;
+	int image_fd;
+	
+	image_fd = open(file_name, O_RDWR | O_CREAT , S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP); 
+	if (image_fd <= 0) 
+		return -1;
+
+	ret = lseek(image_fd, 0, SEEK_SET);
+	if (ret < 0) 
+		return -1;
+
+	towrite = size;
+	ptr = g_image;
+	while (towrite > 0)
+	{
+		n = write(image_fd, ptr, size); 
+		towrite -= n;
+		ptr += n;
+	}
+
+	return 0;
+}
+
+int sync_exit()
+{
+	if (log_file)
+		fprintf (log_file, "SYNC EXIT.\n");
+	close(fd);
+
+	return 0;
+}
+
